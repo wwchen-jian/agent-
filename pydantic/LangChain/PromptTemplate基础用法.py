8e0e55f2d99b575dcd0924256c187debd22c3a0e
wwchen-jian <1725399948@qq.com>
2026-08-01 12:55:32 +0800

    Delete pydantic/LangChain directory

--- pydantic/LangChain/PromptTemplate基础用法.py
+++
@@ -1,9 +0,0 @@
-from langchain_core.prompts import PromptTemplate
-
-template = PromptTemplate(
-    input_variables=['commoidty','price'],
-    template = '请为{commodity}提供一段吸引人的促销词,突出其{price}价格的性价比'
-)
-
-prompt = template.format(commodity = '西瓜' , price = '20')
-print(prompt)
